1ca1d98c2f37a5a2406b8af912ebd48bf4ac17a4
akebrake <ake.ek@live.com>
2023-04-02 15:33:43 +0200

    updated form with validation

--- src/pages/contact1.js
+++ src/pages/contact1.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from "../modules/contactform.module.css";
 import { ToastContainer, toast } from "react-toastify";
 import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import "react-toastify/dist/ReactToastify.css";
 
 function ContactForm1() {
@@ -32,10 +33,11 @@ function ContactForm1() {
     }
   }
 
-  const { register, handleSubmit } = useForm();
+  const { register, formState: { errors }, handleSubmit } = useForm();
   
   function onSubmit(data) {
     console.log(data);
+    alert();
   }
 
     return (
@@ -48,46 +50,64 @@ function ContactForm1() {
                         <label htmlFor="name">Name</label>
                         <input
                             {...register('name', {
-                            required: true,
+                            required: "Minimum 3 characters",
                             minLength: 3,
                             })}
                             value={name}
                             placeholder="Your name"
                             onChange={onTextInputChange}
                         />
+                        <ErrorMessage
+                            errors={errors}
+                            name="name"
+                            render={({ message }) => <p>{message}</p>}
+                        />
                         <label htmlFor="email">Email</label>
                         <input
                             {...register("email", {
+                                required: "Must be a valid email-adress",
                                 pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
                               })}
                             value={email}
                             placeholder="Your email"
                             onChange={onTextInputChange}
                         />
+                        <ErrorMessage
+                            errors={errors}
+                            name="email"
+                            render={({ message }) => <p>{message}</p>}
+                        />
                         <label htmlFor="subject">Subject</label>
                         <input
                             {...register('subject', {
-                                required: true,
+                                required: "Minimum 3 characters",
                                 minLength: 3,
                                 })}
                             value={subject}
                             placeholder="Your subject"
                             onChange={onTextInputChange}
                         />
+                        <ErrorMessage
+                            errors={errors}
+                            name="subject"
+                            render={({ message }) => <p>{message}</p>}
+                        />
                         <label htmlFor="message">Message</label>
                         <input
                             {...register('message', {
-                                required: true,
+                                required: "Minimum 3 characters",
                                 minLength: 3,
                                 })}
                             value={message}
                             placeholder="Your message"
                             onChange={onTextInputChange}
                         />
-                    <button 
-                        className={styles.formBtn} 
-                        onClick={() => {
-                        alert();}}>
+                        <ErrorMessage
+                            errors={errors}
+                            name="message"
+                            render={({ message }) => <p>{message}</p>}
+                        />
+                    <button className={styles.formBtn}>
                         Submit
                     </button>
                     <ToastContainer autoClose={3000} />
